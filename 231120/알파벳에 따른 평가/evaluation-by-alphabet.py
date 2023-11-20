0d57d740a126df083f717ff8f0b4ee4065283516
kdg-04 <102803593+kdg-04@users.noreply.github.com>
2023-11-20 10:32:17 +0900

    Title: 알파벳에 따른 평가, Time: 171ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231120/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+alpha = str(input())
+
+if alpha == 'S':
+    print("Superior")
+elif alpha == 'A':
+    print("Excellent")
+elif alpha == 'B':
+    print("Good")
+elif alpha == 'C':
+    print("Usually")
+elif alpha == 'D':
+    print("Effort")
+else:
+    print("Failure")
