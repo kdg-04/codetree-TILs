6046abaa276d776f5bb4b92920f62960665086d1
kdg-04 <102803593+kdg-04@users.noreply.github.com>
2023-11-22 01:44:11 +0900

    Title: 중앙값 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231122/중앙값 구하기/find-the-median.py
+++ 231122/중앙값 구하기/find-the-median.py
@@ -1,8 +1,20 @@
 x, y, z = map(int, input().split())
 
-if (z >= x >= y) or (y >= x >= z):
-    print(x)
-elif (x >= y >= z) or (z >= y >= x):
-    print(y)
+if a > b:
+    if c > a:
+        # a > b, c > a 일때 (c > a > b)
+        print(a)
+    # a > b, a > c 일때 (a가 가장 크고, b와 c중 더 큰 수가 중앙값)
+    elif b > c:
+        print(b)
+    else:
+        print(c)
 else:
-    print(z)
+    if c > b:
+        # b > a, c > b 일때 (c > b > a)
+        print(b)
+    # b > a, b > c 일때 (b가 가장 크고, a와 c중 더 큰 수가 중앙값)
+    elif a > c:
+        print(a)
+    else:
+        print(c)
