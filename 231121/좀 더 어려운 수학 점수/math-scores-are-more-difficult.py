df94106bc28367168e48aa3df225beede1788f24
kdg-04 <102803593+kdg-04@users.noreply.github.com>
2023-11-21 09:39:19 +0900

    Title: 좀 더 어려운 수학 점수, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 231121/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 231121/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -1,9 +1,7 @@
 ams, aes = map(int, input().split())
 bms, bes = map(int, input().split())
 
-if ams > bms:
-    print("A")
-elif ams == bms and aes > bes:
+if ams > bms or (ams == bms and aes > bes):
     print("A")
 else:
     print("B")
