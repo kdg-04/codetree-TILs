e1e47f3536fda77aed891747e1445591e1093279
KDG <102803593+kdg-04@users.noreply.github.com>
2023-12-09 16:55:06 +0900

    Title: 몫 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231209/몫 출력하기/print-share.py
+++ 231209/몫 출력하기/print-share.py
@@ -1,10 +1,13 @@
 cnt = 0
-
-while cnt != 3:
-    n = int(input())
-
-    if n % 2 != 0:
-        continue
-    else:
-        cnt += 1
-        print(n // 2)
+	
+while 1:
+	n = int(input())
+	
+	if n % 2 != 1:
+		continue
+		
+	print(n // 2)
+	cnt += 1
+		
+	if cnt >= 3:
+		break
