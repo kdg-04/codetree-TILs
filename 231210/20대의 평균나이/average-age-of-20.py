ccb9354fa34c079b75973af6b94768373fc5f3cd
KDG <102803593+kdg-04@users.noreply.github.com>
2023-12-10 17:15:50 +0900

    Title: 20대의 평균나이, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 231210/20대의 평균나이/average-age-of-20.py
+++ 231210/20대의 평균나이/average-age-of-20.py
@@ -1,15 +1,14 @@
 cnt = 0
 total_age = 0
 
-while True:
+while 1:
     age = int(input())
 
-    if age >= 30:
+    if age < 20 or age >= 30:
         break
 
     cnt += 1
     total_age += age
 
 if cnt > 0:
-    average_age = total_age / cnt
-    print("%.2f" % average_age)
+    print("%.2f" % (total_age / cnt))
