178e2a33f4b17da10ca82d6da716b92c819b9aaa
KDG <102803593+kdg-04@users.noreply.github.com>
2024-01-11 01:11:42 +0900

    Title: 소수 판별기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240111/소수 판별기/prime-number-discriminator.py
+++ 240111/소수 판별기/prime-number-discriminator.py
@@ -1,11 +1,10 @@
 n = int(input())
 
-for i in range(1, n):
+for i in range(2, n):
     if n % i == 0:
-        prime_number = 0
-    else:
-        prime_number = 1
-if prime_number == 1:
-    print("P")
-else:
+        prime_number = False
+        
+if prime_number == False:
     print("C")
+else:
+    print("P")
